c8ce25fd5fdad89e32066fa0a76c50414ea38f88
FalseHonesty <thefalsehonesty@gmail.com>
2019-10-12 02:02:41 -0400

    Remove bad import

--- src/main/kotlin/club/sk1er/elementa/constraints/animation/AnimationStrategy.kt
+++ src/main/kotlin/club/sk1er/elementa/constraints/animation/AnimationStrategy.kt
@@ -4,9 +4,6 @@ import kotlin.math.cos
 import kotlin.math.pow
 import kotlin.math.sin
 import kotlin.math.sqrt
-import net.minecraft.realms.Tezzelator.t
-
-
 
 interface AnimationStrategy {
     fun getValue(percentComplete: Float): Float
