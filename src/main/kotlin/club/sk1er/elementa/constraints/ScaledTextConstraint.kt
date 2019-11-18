a1b589fe5caa90f7b4d83a29a4f8f666f31bf4d8
Andrew Koerber <kerbybit@gmail.com>
2019-11-18 00:57:37 -0600

    Added ScaledTextConstraint and updated version number

---
+++ src/main/kotlin/club/sk1er/elementa/constraints/ScaledTextConstraint.kt
@@ -0,0 +1,29 @@
+package club.sk1er.elementa.constraints
+
+import club.sk1er.elementa.UIComponent
+import club.sk1er.elementa.components.UIText
+import net.minecraft.client.Minecraft
+
+/**
+ * Sets the width/height to be a scale of the default text width and height
+ */
+class ScaledTextConstraint(private var scale: Float) : SizeConstraint {
+    override var cachedValue = 0f
+    override var recalculate = true
+
+    private var fontRenderer = Minecraft.getMinecraft().fontRendererObj
+
+    override fun getWidthImpl(component: UIComponent, parent: UIComponent): Float {
+        return when (component) {
+            is UIText -> scale * fontRenderer.getStringWidth(component.getText())
+            else -> throw IllegalAccessException("ScaledTextConstraint can only be used with UIText")
+        }
+    }
+
+    override fun getHeightImpl(component: UIComponent, parent: UIComponent): Float {
+        return when(component) {
+            is UIText -> scale * 9
+            else -> throw IllegalAccessException("ScaledTextConstraint can only be used with UIText")
+        }
+    }
+}
