edddb3b2868d091257078c35158e0f9797a428e0
FalseHonesty <thefalsehonesty@gmail.com>
2020-01-03 18:02:52 -0500

    Shouldn't NPE when child-less

--- src/main/kotlin/club/sk1er/elementa/constraints/ChildBasedConstraints.kt
+++ src/main/kotlin/club/sk1er/elementa/constraints/ChildBasedConstraints.kt
@@ -29,14 +29,14 @@ class ChildBasedMaxSizeConstraint : SizeConstraint {
     override var constrainTo: UIComponent? = null
 
     override fun getWidthImpl(component: UIComponent): Float {
-        return (constrainTo ?: component).children.maxBy{ it.getWidth() }!!.getWidth()
+        return (constrainTo ?: component).children.maxBy { it.getWidth() }?.getWidth() ?: 0f
     }
 
     override fun getHeightImpl(component: UIComponent): Float {
-        return (constrainTo ?: component).children.maxBy{ it.getHeight() }!!.getHeight()
+        return (constrainTo ?: component).children.maxBy { it.getHeight() }?.getHeight() ?: 0f
     }
 
     override fun getRadiusImpl(component: UIComponent): Float {
-        return (constrainTo ?: component).children.maxBy{ it.getHeight() }!!.getHeight() * 2f
+        return (constrainTo ?: component).children.maxBy { it.getHeight() }?.getHeight()?.times(2f) ?: 0f
     }
 }
