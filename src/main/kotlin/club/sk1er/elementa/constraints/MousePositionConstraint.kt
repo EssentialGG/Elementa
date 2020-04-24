6789090af967008aca2898e228bddf585f4a4366
FalseHonesty <thefalsehonesty@gmail.com>
2020-04-24 01:27:35 -0400

    Added MousePositionConstraint

--- src/main/kotlin/club/sk1er/elementa/constraints/MousePositionConstraint.kt
+++ src/main/kotlin/club/sk1er/elementa/constraints/MousePositionConstraint.kt
@@ -1,4 +1,19 @@
 package club.sk1er.elementa.constraints
 
-class MousePositionConstraint {
+import club.sk1er.elementa.UIComponent
+import club.sk1er.mods.core.universal.UniversalMouse
+import club.sk1er.mods.core.universal.UniversalResolutionUtil
+
+class MousePositionConstraint : PositionConstraint {
+    override var cachedValue = 0f
+    override var recalculate = true
+    override var constrainTo: UIComponent? = null
+
+    override fun getXPositionImpl(component: UIComponent): Float {
+        return UniversalMouse.getScaledX().toFloat()
+    }
+
+    override fun getYPositionImpl(component: UIComponent): Float {
+        return UniversalResolutionUtil.getInstance().scaledHeight - UniversalMouse.getScaledY().toFloat()
+    }
 }
