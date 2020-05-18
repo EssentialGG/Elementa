cc6223636619f89c5e5e749907ddfddc92be126f
FalseHonesty <thefalsehonesty@gmail.com>
2020-05-18 13:56:57 -0400

    Add DSL utilities to easily create basic custom constraints

--- src/main/kotlin/club/sk1er/elementa/dsl/constraints.kt
+++ src/main/kotlin/club/sk1er/elementa/dsl/constraints.kt
@@ -1,5 +1,6 @@
 package club.sk1er.elementa.dsl
 
+import club.sk1er.elementa.UIComponent
 import club.sk1er.elementa.constraints.*
 
 infix fun SuperConstraint<Float>.min(minConstraint: SuperConstraint<Float>) =
@@ -16,3 +17,35 @@ operator fun SuperConstraint<Float>.plus(other: SuperConstraint<Float>) =
 
 operator fun SuperConstraint<Float>.minus(other: SuperConstraint<Float>) =
     SubtractiveConstraint(this, other)
+
+fun basicWidthConstraint(calculator: (component: UIComponent) -> Float) = object : WidthConstraint {
+    override var cachedValue = 0f
+    override var recalculate = true
+    override var constrainTo: UIComponent? = null
+
+    override fun getWidthImpl(component: UIComponent) = calculator(component)
+}
+
+fun basicHeightConstraint(calculator: (component: UIComponent) -> Float) = object : HeightConstraint {
+    override var cachedValue = 0f
+    override var recalculate = true
+    override var constrainTo: UIComponent? = null
+
+    override fun getHeightImpl(component: UIComponent) = calculator(component)
+}
+
+fun basicXConstraint(calculator: (component: UIComponent) -> Float) = object : XConstraint {
+    override var cachedValue = 0f
+    override var recalculate = true
+    override var constrainTo: UIComponent? = null
+
+    override fun getXPositionImpl(component: UIComponent) = calculator(component)
+}
+
+fun basicYConstraint(calculator: (component: UIComponent) -> Float) = object : YConstraint {
+    override var cachedValue = 0f
+    override var recalculate = true
+    override var constrainTo: UIComponent? = null
+
+    override fun getYPositionImpl(component: UIComponent) = calculator(component)
+}
