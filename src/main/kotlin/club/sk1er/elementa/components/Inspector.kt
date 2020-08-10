36fc6a9e958eca424e14c189199dd6b8cb69a385
FalseHonesty <thefalsehonesty@gmail.com>
2020-08-10 16:03:10 -0400

    Inspector: Implement component hit testing & hidden checking
    Now, the Inspector element has a button on the titlebar that, when
    clicked, will allow the user to move their mouse across the screen,
    drawing the outline box over the currently hovered component.
    
    Additionally, this patch changes the Inspector's tree view arrows
    to be plus/minus boxes, which is more commonly seen in tree views.
    
    Finally, this patch also adds text to an InspectorNode to indicate
    that a component is hidden.

--- src/main/kotlin/club/sk1er/elementa/components/Inspector.kt
+++ src/main/kotlin/club/sk1er/elementa/components/Inspector.kt
@@ -2,9 +2,10 @@ package club.sk1er.elementa.components
 
 import club.sk1er.elementa.UIComponent
 import club.sk1er.elementa.constraints.*
-import club.sk1er.elementa.constraints.animation.Animations
 import club.sk1er.elementa.dsl.*
 import club.sk1er.elementa.effects.OutlineEffect
+import club.sk1er.mods.core.universal.UniversalMouse
+import club.sk1er.mods.core.universal.UniversalResolutionUtil
 import java.awt.Color
 
 class Inspector(
@@ -20,6 +21,8 @@ class Inspector(
 
     private val outlineEffect = OutlineEffect(outlineColor, outlineWidth)
 
+    private var isClickSelecting = false
+
     init {
         constrain {
             width = ChildBasedSizeConstraint()
@@ -34,7 +37,7 @@ class Inspector(
         val titleBlock = UIContainer().constrain {
             x = CenterConstraint()
             width = ChildBasedSizeConstraint() + 20.pixels()
-            height = ChildBasedSizeConstraint() + 20.pixels()
+            height = ChildBasedMaxSizeConstraint() + 20.pixels()
         }.onMouseClick {
             clickPos = if (it.relativeX < 0 || it.relativeY < 0 || it.relativeX > getWidth() || it.relativeY > getHeight()) {
                 null
@@ -55,11 +58,22 @@ class Inspector(
             }
         } childOf container
 
-        UIText("Inspector").constrain {
+        val title = UIText("Inspector").constrain {
             x = CenterConstraint()
             y = CenterConstraint()
             width = TextAspectConstraint()
             height = 14.pixels()
+        } childOf titleBlock
+
+        SVGComponent.ofResource("/svg/click.svg").constrain {
+            x = SiblingConstraint(10f)
+            y = CenterConstraint()
+            width = AspectConstraint(1f)
+            height = RelativeConstraint(1f).to(title) as HeightConstraint
+        }.onMouseClick { event ->
+            event.stopPropagation()
+
+            isClickSelecting = true
         } childOf titleBlock
 
         val treeBlock = UIBlock(backgroundColor).constrain {
@@ -85,9 +99,20 @@ class Inspector(
     }
 
     override fun draw() {
-        super.draw()
+        if (isClickSelecting) {
+            val res = Window.of(rootNode.targetComponent).scaledResolution
+            val mouseX = UniversalMouse.getScaledX().toFloat()
+            val mouseY = res.scaledHeight - UniversalMouse.getTrueY() * res.scaledHeight /
+                    UniversalResolutionUtil.getInstance().windowHeight - 1f
+            val hitComponent = rootNode.targetComponent.hitTest(mouseX, mouseY)
 
-        selectedComponent?.also {
+            // TODO: Implement some kind of way to hook into a UIComponent to intercept events,
+            //  allowing for us to stop clicks when in the selecting mode.
+            if (hitComponent == this || hitComponent.isChildOf(this)) null
+            else hitComponent
+        } else {
+            selectedComponent
+        }?.also {
             UIBlock.drawBlock(
                 Color(129, 212, 250, 100),
                 it.getLeft().toDouble(),
@@ -96,14 +121,16 @@ class Inspector(
                 it.getBottom().toDouble()
             )
         }
+
+        super.draw()
     }
 
     class InspectorArrow(private val empty: Boolean) : TreeArrowComponent() {
-        private val closedIcon = SVGComponent.ofResource("/svg/square-minus.svg").constrain {
+        private val closedIcon = SVGComponent.ofResource("/svg/square-plus.svg").constrain {
             width = 10.pixels()
             height = 10.pixels()
         }
-        private val openIcon = SVGComponent.ofResource("/svg/square-plus.svg").constrain {
+        private val openIcon = SVGComponent.ofResource("/svg/square-minus.svg").constrain {
             width = 10.pixels()
             height = 10.pixels()
         }
@@ -129,11 +156,30 @@ class Inspector(
         }
     }
 
-    inner class InspectorNode(private val targetComponent: UIComponent) : TreeNode() {
+    inner class InspectorNode(val targetComponent: UIComponent) : TreeNode() {
         override var arrowComponent: () -> TreeArrowComponent = { InspectorArrow(targetComponent.children.isEmpty()) }
 
         override fun toComponent(): UIComponent {
-            return UIText(targetComponent.javaClass.simpleName).constrain {
+            val componentName = targetComponent.javaClass.simpleName
+            var wasHidden = false
+
+            return object : UIText(componentName) {
+                override fun animationFrame() {
+                    super.animationFrame()
+
+                    val isCurrentlyHidden = targetComponent.parent != targetComponent && !targetComponent.parent.children.contains(
+                        targetComponent
+                    )
+                    if (isCurrentlyHidden && !wasHidden) {
+                        wasHidden = true
+                        setText("§r$componentName §7§o(Hidden)")
+                    } else if (!isCurrentlyHidden && wasHidden) {
+                        wasHidden = false
+                        setText(componentName)
+                    }
+                }
+
+            }.constrain {
                 x = SiblingConstraint() + 5.pixels()
                 width = TextAspectConstraint()
             }.onMouseClick { event ->
