6da4f3fd5955f5cf75b90593ee2c5eae6ee2d61d
Matthew Olsson <matthewcolsson@gmail.com>
2020-08-13 16:43:05 -0700

    Inspector: Add values tab to show selected component width and position

--- src/main/kotlin/club/sk1er/elementa/components/Inspector.kt
+++ src/main/kotlin/club/sk1er/elementa/components/Inspector.kt
@@ -19,7 +19,7 @@ class Inspector(
 ) : UIContainer() {
     private val rootNode = componentToNode(rootComponent)
     private val treeBlock: UIContainer
-    private val treeView: TreeView
+    private lateinit var treeView: TreeView
     private val container: UIComponent
     private var selectedNode: InspectorNode? = null
     private val infoBlockScroller: ScrollComponent
@@ -115,7 +115,7 @@ class Inspector(
 
         val infoBlock = InfoBlock().constrain {
             y = SiblingConstraint()
-            width = ChildBasedSizeConstraint() + 10.pixels()
+            width = ChildBasedMaxSizeConstraint() + 10.pixels()
             height = ChildBasedSizeConstraint() + 10.pixels()
         }
 
@@ -262,10 +262,128 @@ class Inspector(
 
     inner class InfoBlock : UIContainer() {
         private var cachedComponent: UIComponent? = null
-        private val constraintsTree = TreeView().constrain {
+
+        private val tabContainer = UIContainer().constrain {
+            width = ChildBasedSizeConstraint() + 15.pixels()
+            height = ChildBasedMaxSizeConstraint() + 10.pixels()
+        } childOf this
+
+        private val contentContainer = UIContainer().constrain {
+            y = SiblingConstraint()
+            width = ChildBasedSizeConstraint()
+            height = ChildBasedSizeConstraint()
+        } childOf this
+
+        private val constraintsContent = TreeView().constrain {
             x = 5.pixels()
             y = 5.pixels()
-        } childOf this
+            width = ChildBasedMaxSizeConstraint()
+            height = ChildBasedSizeConstraint()
+        } childOf contentContainer
+
+        private val valuesContent = UIContainer().constrain {
+            x = 20.pixels()
+            y = 5.pixels()
+            width = ChildBasedMaxSizeConstraint()
+            height = ChildBasedSizeConstraint()
+        }
+
+        private lateinit var constraintsText: UIText
+        private lateinit var valuesText: UIText
+
+        private val xValueText: UIText
+        private val yValueText: UIText
+        private val widthValueText: UIText
+        private val heightValueText: UIText
+
+        private var constraintsSelected = true
+
+        init {
+            constraintsText = UIText("Constraints").constrain {
+                x = 5.pixels()
+                y = 5.pixels()
+                width = TextAspectConstraint()
+                color = Color.WHITE.asConstraint()
+            }.onMouseEnter {
+                if (!constraintsSelected) {
+                    constraintsText.animate {
+                        setColorAnimation(Animations.OUT_EXP, 0.5f, Color.WHITE.asConstraint())
+                    }
+                }
+            }.onMouseLeave {
+                if (!constraintsSelected) {
+                    constraintsText.animate {
+                        setColorAnimation(Animations.OUT_EXP, 0.5f, Color(255, 255, 255, 102).asConstraint())
+                    }
+                }
+            }.onMouseClick {
+                if (!constraintsSelected) {
+                    constraintsSelected = true
+                    contentContainer.removeChild(valuesContent)
+                    contentContainer.addChild(constraintsContent)
+                    valuesText.animate {
+                        setColorAnimation(Animations.OUT_EXP, 0.5f, Color(255, 255, 255, 102).asConstraint())
+                    }
+                }
+            } as UIText childOf tabContainer
+
+            valuesText = UIText("Values").constrain {
+                x = SiblingConstraint(10f)
+                y = 5.pixels()
+                width = TextAspectConstraint()
+                color = Color(255, 255, 255, 102).asConstraint()
+            }.onMouseEnter {
+                if (constraintsSelected) {
+                    valuesText.animate {
+                        setColorAnimation(Animations.OUT_EXP, 0.5f, Color.WHITE.asConstraint())
+                    }
+                }
+            }.onMouseLeave {
+                if (constraintsSelected) {
+                    valuesText.animate {
+                        setColorAnimation(Animations.OUT_EXP, 0.5f, Color(255, 255, 255, 102).asConstraint())
+                    }
+                }
+            }.onMouseClick {
+                if (constraintsSelected) {
+                    constraintsSelected = false
+                    contentContainer.removeChild(constraintsContent)
+                    contentContainer.addChild(valuesContent)
+                    constraintsText.animate {
+                        setColorAnimation(Animations.OUT_EXP, 0.5f, Color(255, 255, 255, 102).asConstraint())
+                    }
+                }
+            } as UIText childOf tabContainer
+
+            xValueText = UIText("0")
+            yValueText = UIText("0")
+            widthValueText = UIText("0")
+            heightValueText = UIText("0")
+
+            initializeText("x", xValueText) childOf valuesContent
+            initializeText("y", yValueText) childOf valuesContent
+            initializeText("width", widthValueText) childOf valuesContent
+            initializeText("height", heightValueText) childOf valuesContent
+        }
+
+        private fun initializeText(name: String, valueText: UIText): UIContainer {
+            val container = UIContainer().constrain {
+                y = SiblingConstraint()
+                width = ChildBasedSizeConstraint()
+                height = ChildBasedMaxSizeConstraint() + 3.pixels()
+            }
+
+            UIText("$name: ").constrain {
+                width = TextAspectConstraint()
+            } childOf container
+
+            valueText.constrain {
+                x = SiblingConstraint()
+                width = TextAspectConstraint()
+            }  childOf container
+
+            return container
+        }
 
         private fun setNewConstraints(constraints: UIConstraints) {
             setConstraintNodes(constraints)
@@ -299,7 +417,7 @@ class Inspector(
                     nodes.add(getNodeFromConstraint(it, "Color"))
             }
 
-            constraintsTree.setRoots(nodes)
+            constraintsContent.setRoots(nodes)
         }
 
         private fun getNodeFromConstraint(constraint: SuperConstraint<*>, name: String? = null): TreeNode {
@@ -364,6 +482,13 @@ class Inspector(
                     }
                 }
             }
+
+            if (!constraintsSelected && cachedComponent != null) {
+                xValueText.setText("%.2f".format(cachedComponent!!.getLeft()))
+                yValueText.setText("%.2f".format(cachedComponent!!.getTop()))
+                widthValueText.setText("%.2f".format(cachedComponent!!.getWidth()))
+                heightValueText.setText("%.2f".format(cachedComponent!!.getHeight()))
+            }
         }
 
         private fun constraintHasChildren(constraint: SuperConstraint<*>) = when (constraint) {
@@ -392,7 +517,7 @@ class Inspector(
                         x = SiblingConstraint()
                     } childOf this
 
-                    val strings = when (constraint) {
+                    val properties = when (constraint) {
                         is AlphaAspectColorConstraint -> listOf(constraint::color, constraint::value)
                         is AspectConstraint -> listOf(constraint::value)
                         is ChildBasedSizeConstraint -> listOf(constraint::padding)
@@ -410,8 +535,11 @@ class Inspector(
                         else -> listOf()
                     }
 
-                    fun toString(o: Any) =
-                        if (o is Color) "Color(${o.red}, ${o.green}, ${o.blue}, ${o.alpha})" else o.toString()
+                    fun toString(o: Any) = when (o) {
+                        is Color -> "Color(${o.red}, ${o.green}, ${o.blue}, ${o.alpha})"
+                        is Double, is Float -> "%.2f".format(o)
+                        else -> o.toString()
+                    }
 
                     stringHolder = UIContainer().constrain {
                         x = 13.pixels()
@@ -427,7 +555,7 @@ class Inspector(
                         createStringComponent("§7Paused: ${constraint.animationPaused}§r")
                     }
 
-                    strings.forEach {
+                    properties.forEach {
                         createStringComponent("§7${it.name}: ${toString(it.get())}§r")
                     }
                 }
