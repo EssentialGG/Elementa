c59bf08b219e34b156d1833f3a93ca633f240e88
Matthew Olsson <matthewcolsson@gmail.com>
2020-08-29 17:44:37 -0700

    MarkdownComponent: Add ScrollComponent, scissor text drawing

--- src/main/kotlin/club/sk1er/elementa/markdown/MarkdownComponent.kt
+++ src/main/kotlin/club/sk1er/elementa/markdown/MarkdownComponent.kt
@@ -1,13 +1,23 @@
 package club.sk1er.elementa.markdown
 
 import club.sk1er.elementa.UIComponent
+import club.sk1er.elementa.components.ScrollComponent
+import club.sk1er.elementa.components.UIBlock
+import club.sk1er.elementa.components.UIContainer
+import club.sk1er.elementa.constraints.CenterConstraint
+import club.sk1er.elementa.constraints.FillConstraint
+import club.sk1er.elementa.constraints.RelativeConstraint
+import club.sk1er.elementa.dsl.*
+import club.sk1er.elementa.effects.ScissorEffect
+import java.awt.Color
 
 /**
  * Component that parses a string as Markdown and renders it.
  *
  * This component's width and height must be non-child-related
- * constraints. This is because this component will not have any
- * children, as all rendering is done directly via rendering calls.
+ * constraints. This is because the actual text rendering is
+ * done with direct render calls instead of through the component
+ * hierarchy.
  */
 class MarkdownComponent(
     text: String,
@@ -15,13 +25,59 @@ class MarkdownComponent(
 ) : UIComponent() {
     private val document = Document.fromString(text)
 
+    private val scrollComponent = ScrollComponent().constrain {
+        width = RelativeConstraint() - 15.pixels()
+        height = RelativeConstraint()
+    } childOf this
+
+    private val scrollChild = UIContainer().constrain {
+        width = RelativeConstraint()
+        height = 0.pixels()
+    } childOf scrollComponent
+
+    private val scissor = ScissorEffect(scrollComponent)
+
+    init {
+        val scrollBar = UIContainer().constrain {
+            x = 2.pixels(alignOpposite = true)
+            width = 10.pixels()
+            height = FillConstraint()
+        } childOf this
+
+        val scrollBarContainer = UIContainer().constrain {
+            x = CenterConstraint()
+            y = 2.pixels()
+            width = 3.pixels()
+            height = RelativeConstraint() - 4.pixels()
+        } childOf scrollBar
+
+        val scrollBarGrip = UIBlock(Color(180, 180, 180, 180)).constrain {
+            x = CenterConstraint()
+            y = 0.pixels()
+            width = RelativeConstraint()
+            height = 30.pixels()
+        } childOf scrollBarContainer
+
+        scrollComponent.setScrollBarComponent(scrollBarGrip, hideWhenUseless = true)
+    }
+
     override fun draw() {
-        document?.draw(MarkdownState(
+        super.draw()
+
+        scissor.beforeDraw(this)
+
+        val state = MarkdownState(
             getLeft(),
-            getTop(),
-            getWidth(),
+            scrollChild.getTop(),
+            scrollChild.getWidth(),
             getHeight(),
             config = config
-        ))
+        )
+
+        document?.draw(state)
+
+        scrollChild.setHeight(state.y.pixels())
+
+        scissor.afterDraw(this)
     }
 }
