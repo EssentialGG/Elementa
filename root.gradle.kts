8b36f16169b99e969437140909b5ce50742b9e0b
Matthew Olsson <matthewcolsson@gmail.com>
2020-09-18 09:18:00 -0700

    Remove 1.16.2-fabric until it can be implemented

--- root.gradle.kts
+++ root.gradle.kts
@@ -1,5 +1,3 @@
-
-
 plugins {
     kotlin("jvm") version "1.3.72" apply false
     id("fabric-loom") version "0.4-SNAPSHOT" apply false
@@ -13,7 +11,7 @@ plugins {
 configurations.register("compileClasspath")
 
 preprocess {
-    "1.16.2-fabric"(11602, "yarn") {
+//    "1.16.2-fabric"(11602, "yarn") {
         "1.16.2"(11602, "srg") {
             "1.15.2"(11502, "srg") {
                 "1.12.2"(11202, "srg", file("versions/1.15.2-1.12.2.txt")) {
@@ -21,6 +19,6 @@ preprocess {
                 }
             }
         }
-    }
+//    }
 }
 
