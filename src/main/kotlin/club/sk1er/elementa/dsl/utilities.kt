0f0a61986fc50df370b38a33bdb835d20ba7d6fc
FalseHonesty <thefalsehonesty@gmail.com>
2020-09-26 15:39:18 -0400

    Add Number#percent utility for RelativeConstraint
    For example: 15.percent() <-> RelativeConstraint(.15f)

--- src/main/kotlin/club/sk1er/elementa/dsl/utilities.kt
+++ src/main/kotlin/club/sk1er/elementa/dsl/utilities.kt
@@ -2,6 +2,7 @@ package club.sk1er.elementa.dsl
 
 import club.sk1er.elementa.constraints.ConstantColorConstraint
 import club.sk1er.elementa.constraints.PixelConstraint
+import club.sk1er.elementa.constraints.RelativeConstraint
 import club.sk1er.mods.core.universal.UniversalGraphicsHandler
 import net.minecraft.client.Minecraft
 import java.awt.Color
@@ -14,5 +15,7 @@ fun String.width(textScale: Float = 1f) = UniversalGraphicsHandler.getStringWidt
 fun Number.pixels(alignOpposite: Boolean = false, alignOutside: Boolean = false): PixelConstraint {
     return PixelConstraint(this.toFloat(), alignOpposite, alignOutside)
 }
+
+fun Number.percent() = RelativeConstraint(this.toFloat() / 100f)
 
 fun Color.asConstraint() = ConstantColorConstraint(this)
