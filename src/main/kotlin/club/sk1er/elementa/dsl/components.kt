8a2c0ad5769e5d82e2f640a0d65bd81117708d52
Matthew Olsson <matthewcolsson@gmail.com>
2021-01-01 20:52:54 -0800

    UIComponent: Add a basic delegate provider which sets componentName

--- src/main/kotlin/club/sk1er/elementa/dsl/components.kt
+++ src/main/kotlin/club/sk1er/elementa/dsl/components.kt
@@ -3,6 +3,8 @@ package club.sk1er.elementa.dsl
 import club.sk1er.elementa.UIComponent
 import club.sk1er.elementa.UIConstraints
 import club.sk1er.elementa.effects.Effect
+import kotlin.properties.Delegates
+import kotlin.reflect.KProperty
 
 fun <T : UIComponent> T.constrain(config: UIConstraints.() -> Unit) = apply {
     constraints.config()
@@ -14,4 +16,11 @@ infix fun <T : UIComponent> T.childOf(parent: UIComponent) = apply {
 
 infix fun <T : UIComponent> T.effect(effect: Effect) = apply {
     this.enableEffect(effect)
+}
+
+operator fun <T : UIComponent> T.provideDelegate(
+    thisRef: Any?,
+    property: KProperty<*>
+) = Delegates.observable(this.also { componentName = property.name }) { _, _, value ->
+    value.componentName = property.name
 }
