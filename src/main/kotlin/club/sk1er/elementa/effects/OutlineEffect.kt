515a92b7922f3fcd12405faa046a212b53440038
Matthew Olsson <matthewcolsson@gmail.com>
2021-01-16 16:41:47 -0800

    OutlineEffect: Use State for color and width

--- src/main/kotlin/club/sk1er/elementa/effects/OutlineEffect.kt
+++ src/main/kotlin/club/sk1er/elementa/effects/OutlineEffect.kt
@@ -1,6 +1,8 @@
 package club.sk1er.elementa.effects
 
 import club.sk1er.elementa.components.UIBlock
+import club.sk1er.elementa.state.BasicState
+import club.sk1er.elementa.state.State
 import club.sk1er.elementa.utils.guiHint
 import java.awt.Color
 
@@ -10,8 +12,8 @@ import java.awt.Color
  * so all children will render above the outline.
  */
 class OutlineEffect @JvmOverloads constructor(
-    var color: Color,
-    var width: Float,
+    color: Color,
+    width: Float,
     var drawAfterChildren: Boolean = false,
     sides: Set<Side> = setOf(Side.Left, Side.Top, Side.Right, Side.Bottom)
 ) : Effect() {
@@ -19,6 +21,29 @@ class OutlineEffect @JvmOverloads constructor(
     private var hasTop = Side.Top in sides
     private var hasRight = Side.Right in sides
     private var hasBottom = Side.Bottom in sides
+
+    private var colorState: State<Color> = BasicState(color)
+    private var widthState: State<Float> = BasicState(width)
+
+    var color: Color
+        get() = colorState.get()
+        set(value) {
+            colorState.set(value)
+        }
+
+    var width: Float
+        get() = widthState.get()
+        set(value) {
+            widthState.set(value)
+        }
+
+    fun bindColor(state: State<Color>) = apply {
+        colorState = state
+    }
+
+    fun bindWidth(state: State<Float>) = apply {
+        widthState = state
+    }
 
     var sides = sides
         set(value) {
@@ -48,6 +73,9 @@ class OutlineEffect @JvmOverloads constructor(
     }
 
     private fun drawOutline() {
+        val color = colorState.get()
+        val width = widthState.get()
+
         val left = boundComponent.getLeft().toDouble()
         val right = boundComponent.getRight().toDouble()
         val top = boundComponent.getTop().toDouble()
