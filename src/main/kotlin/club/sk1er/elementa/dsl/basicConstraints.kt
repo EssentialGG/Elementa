9fc75e1aa6ff1fbbe7fb525d0b3af9603951de22
Matthew Olsson <matthewcolsson@gmail.com>
2021-01-18 13:20:33 -0800

    DSL: Move basicConstraints into their own file

---
+++ src/main/kotlin/club/sk1er/elementa/dsl/basicConstraints.kt
@@ -0,0 +1,46 @@
+package club.sk1er.elementa.dsl
+
+import club.sk1er.elementa.UIComponent
+import club.sk1er.elementa.constraints.*
+import club.sk1er.elementa.constraints.resolution.ConstraintVisitor
+import java.awt.Color
+
+abstract class BasicConstraint<T>(defaultValue: T) : SuperConstraint<T> {
+    override var cachedValue = defaultValue
+    override var recalculate = true
+    override var constrainTo: UIComponent? = null
+
+    override fun visitImpl(visitor: ConstraintVisitor, type: ConstraintType) {}
+}
+
+fun basicXConstraint(calculator: (component: UIComponent) -> Float): XConstraint =
+    object : BasicConstraint<Float>(0f), XConstraint {
+        override fun getXPositionImpl(component: UIComponent) = calculator(component)
+    }
+
+fun basicYConstraint(calculator: (component: UIComponent) -> Float): YConstraint =
+    object : BasicConstraint<Float>(0f), YConstraint {
+        override fun getYPositionImpl(component: UIComponent) = calculator(component)
+    }
+
+fun basicWidthConstraint(calculator: (component: UIComponent) -> Float): WidthConstraint =
+    object : BasicConstraint<Float>(0f), WidthConstraint {
+        override fun getWidthImpl(component: UIComponent) = calculator(component)
+    }
+
+fun basicHeightConstraint(calculator: (component: UIComponent) -> Float): HeightConstraint =
+    object : BasicConstraint<Float>(0f), HeightConstraint {
+        override fun getHeightImpl(component: UIComponent) = calculator(component)
+    }
+
+fun basicRadiusConstraint(calculator: (component: UIComponent) -> Float): RadiusConstraint =
+    object : BasicConstraint<Float>(0f), RadiusConstraint {
+        override fun getRadiusImpl(component: UIComponent) = calculator(component)
+    }
+
+fun basicTextScaleConstraint(calculator: (component: UIComponent) -> Float) = basicHeightConstraint(calculator)
+
+fun basicColorConstraint(calculator: (component: UIComponent) -> Color): ColorConstraint =
+    object : BasicConstraint<Color>(Color.WHITE), ColorConstraint {
+        override fun getColorImpl(component: UIComponent) = calculator(component)
+    }
