0e2d6cd93b42bdfc268705b7e0262e0b4aefa920
Matthew Olsson <matthewcolsson@gmail.com>
2021-01-18 13:21:12 -0800

    DSL: Add componentConstraints, which delegate to a given component
    Instead of writing something like:
    
        component.constrain {
            width = basicWidthConstraint { otherComponent.getWidth() }
        }
    
    One can now write:
    
        component.constrain {
            width = componentWidthConstraint(otherComponent)
        }

---
+++ src/main/kotlin/club/sk1er/elementa/dsl/componentConstraints.kt
@@ -0,0 +1,46 @@
+package club.sk1er.elementa.dsl
+
+import club.sk1er.elementa.UIComponent
+import club.sk1er.elementa.constraints.*
+import club.sk1er.elementa.constraints.resolution.ConstraintVisitor
+import java.awt.Color
+
+abstract class ComponentConstraint<T>(defaultValue: T) : SuperConstraint<T> {
+    override var cachedValue = defaultValue
+    override var recalculate = true
+    override var constrainTo: UIComponent? = null
+
+    override fun visitImpl(visitor: ConstraintVisitor, type: ConstraintType) {}
+}
+
+fun componentXConstraint(boundComponent: UIComponent): XConstraint =
+    object : ComponentConstraint<Float>(0f), XConstraint {
+        override fun getXPositionImpl(component: UIComponent) = boundComponent.getLeft() - boundComponent.parent.getLeft()
+    }
+
+fun componentYConstraint(boundComponent: UIComponent): YConstraint =
+    object : ComponentConstraint<Float>(0f), YConstraint {
+        override fun getYPositionImpl(component: UIComponent) = boundComponent.getTop() - boundComponent.parent.getTop()
+    }
+
+fun componentWidthConstraint(boundComponent: UIComponent): WidthConstraint =
+    object : ComponentConstraint<Float>(0f), WidthConstraint {
+        override fun getWidthImpl(component: UIComponent) = boundComponent.getWidth()
+    }
+
+fun componentHeightConstraint(boundComponent: UIComponent): HeightConstraint =
+    object : ComponentConstraint<Float>(0f), HeightConstraint {
+        override fun getHeightImpl(component: UIComponent) = boundComponent.getHeight()
+    }
+
+fun componentRadiusConstraint(boundComponent: UIComponent): RadiusConstraint =
+    object : ComponentConstraint<Float>(0f), RadiusConstraint {
+        override fun getRadiusImpl(component: UIComponent) = boundComponent.getRadius()
+    }
+
+fun componentTextScaleConstraint(boundComponent: UIComponent) = componentHeightConstraint(boundComponent)
+
+fun componentColorConstraint(boundComponent: UIComponent): ColorConstraint =
+    object : ComponentConstraint<Color>(Color.WHITE), ColorConstraint {
+        override fun getColorImpl(component: UIComponent) = boundComponent.getColor()
+    }
