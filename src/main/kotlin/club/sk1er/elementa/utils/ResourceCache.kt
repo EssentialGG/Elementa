e7b2a339908fb4c8173932f8388c7dea9740a9f8
Jonas Herzig <jonas@spark-squared.com>
2021-05-17 16:56:12 +0200

    UIImage: Fix cache being corrupted due to incorrect fetch
    By using `compute` the cached entry was being replaced by newly created ones,
    even though it was only meant to be retrieved and not replaced.
    This caused assets for which multiple (2 or more) UIImages were requested before
    the asset was loaded to never fully load because any image after the second one
    used the wrong cached instance.

--- src/main/kotlin/club/sk1er/elementa/utils/ResourceCache.kt
+++ src/main/kotlin/club/sk1er/elementa/utils/ResourceCache.kt
@@ -4,7 +4,6 @@ import club.sk1er.elementa.components.UIImage
 import java.awt.image.BufferedImage
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.ConcurrentHashMap
-import java.util.function.BiFunction
 import javax.imageio.ImageIO
 
 class ResourceCache(val size: Int = 50) {
@@ -13,18 +12,14 @@ class ResourceCache(val size: Int = 50) {
     fun get(path: String): UIImage {
         if (cacheMap.size > size)
             cacheMap.clear()
-        return cacheMap.compute(path, BiFunction { pth, current ->
-            if (current != null) {
-                val tmp = CompletableFuture<BufferedImage>()
-                tmp.complete(null)
-                val uiImage = UIImage(tmp)
-                current.supply(uiImage)
-                return@BiFunction uiImage
-            }
+        val cachedImage = cacheMap.computeIfAbsent(path) { pth ->
             UIImage(CompletableFuture.supplyAsync {
                 ImageIO.read(this::class.java.getResourceAsStream(pth))
             })
-        })!!
+        }
+        return UIImage(CompletableFuture.completedFuture<BufferedImage>(null)).also {
+            cachedImage.supply(it)
+        }
     }
 
     fun invalidateAll() {
