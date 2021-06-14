8a111f7efcad522b54e806918845a2ffceed9260
Jonas Herzig <jonas@spark-squared.com>
2021-06-14 18:32:14 +0200

    Build: Use new preprocessor DSL
    Easier to read/write and diff-friendly.

--- root.gradle.kts
+++ root.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "1.5.10" apply false
     id("fabric-loom") version "0.8-SNAPSHOT" apply false
-    id("com.replaymod.preprocess") version "e4476a6"
+    id("com.replaymod.preprocess") version "4b4dfe5"
 }
 
 
@@ -11,16 +11,15 @@ plugins {
 configurations.register("compileClasspath")
 
 preprocess {
-//    "1.16.2-fabric"(11602, "yarn") {
-        "1.16.2"(11602, "srg") {
-            "1.15.2"(11502, "srg") {
-                "1.12.2"(11202, "srg", file("versions/1.15.2-1.12.2.txt")) {
-                    "1.8.9"(10809, "srg", file("versions/1.12.2-1.8.9.txt"))
-                }
-            }
-            "1.16.2-fabric"(11602, "yarn") {
-            }
-        }
-//    }
+    val fabric11602 = createNode("1.16.2-fabric", 11602, "yarn")
+    val forge11602 = createNode("1.16.2", 11602, "srg")
+    val forge11502 = createNode("1.15.2", 11502, "srg")
+    val forge11202 = createNode("1.12.2", 11202, "srg")
+    val forge10809 = createNode("1.8.9", 10809, "srg")
+
+    fabric11602.link(forge11602)
+    forge11602.link(forge11502)
+    forge11502.link(forge11202, file("versions/1.15.2-1.12.2.txt"))
+    forge11202.link(forge10809, file("versions/1.12.2-1.8.9.txt"))
 }
 
