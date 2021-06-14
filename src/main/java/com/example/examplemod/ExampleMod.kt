3a2a1a37fb355704c4b476f241194d3830cf3881
Jonas Herzig <jonas@spark-squared.com>
2021-06-14 16:42:29 +0200

    ExampleMod: Open screen right in main menu
    Cause having the enter SP to test stuff is too slow. And the command
    implementations are a bit concerning wrt thread-safety.

--- src/main/java/com/example/examplemod/ExampleMod.kt
+++ src/main/java/com/example/examplemod/ExampleMod.kt
@@ -2,11 +2,10 @@ package com.example.examplemod
 
 import gg.essential.elementa.effects.StencilEffect
 import gg.essential.universal.UMinecraft
-import gg.essential.universal.UScreen
+import net.minecraft.client.gui.GuiMainMenu
 
 //#if FORGE
 //#if MC<=11202
-import net.minecraftforge.client.ClientCommandHandler
 import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.Mod.EventHandler
@@ -19,17 +18,11 @@ import net.minecraftforge.fml.common.gameevent.TickEvent
 //$$ import net.minecraftforge.common.MinecraftForge
 //$$ import net.minecraftforge.fml.common.Mod
 //$$
-//#if MC>=11602
-//$$ import net.minecraftforge.event.RegisterCommandsEvent
-//#else
-//$$ import net.minecraftforge.fml.event.server.FMLServerStartingEvent
-//#endif
 //#endif
 //#else
 //#if FABRIC
 //$$ import net.fabricmc.api.ModInitializer
 //$$ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
-//$$ import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback
 //#endif
 //#endif
 
@@ -47,10 +40,6 @@ class ExampleMod {
     //$$ override fun onInitialize() {
     //$$     StencilEffect.enableStencil()
     //$$     ClientTickEvents.START_CLIENT_TICK.register { tick() }
-    //$$     CommandRegistrationCallback.EVENT.register { dispatcher, dedicated ->
-    //$$         ExampleCommand.register(dispatcher)
-    //$$         ComponentsCommand.register(dispatcher)
-    //$$     }
     //$$ }
     //#else
     //#if MC<=11202
@@ -58,8 +47,6 @@ class ExampleMod {
     fun init(event: FMLInitializationEvent) {
         StencilEffect.enableStencil()
         MinecraftForge.EVENT_BUS.register(this)
-        ClientCommandHandler.instance.registerCommand(ExampleCommand())
-        ClientCommandHandler.instance.registerCommand(ComponentsCommand())
     }
     //#else
     //$$ init {
@@ -67,19 +54,6 @@ class ExampleMod {
     //$$     MinecraftForge.EVENT_BUS.register(this)
     //$$ }
     //$$
-    //#if MC>=11602
-    //$$ @SubscribeEvent
-    //$$ fun registerCommands(event: RegisterCommandsEvent) {
-    //$$     ExampleCommand.register(event.dispatcher)
-    //$$     ComponentsCommand.register(event.dispatcher)
-    //$$ }
-    //#else
-    //$$ @SubscribeEvent
-    //$$ fun serverStarting(event: FMLServerStartingEvent) {
-    //$$     ExampleCommand.register(event.commandDispatcher)
-    //$$     ComponentsCommand.register(event.commandDispatcher)
-    //$$ }
-    //#endif
     //#endif
 
     @SubscribeEvent
@@ -87,19 +61,13 @@ class ExampleMod {
     //#endif
 
     private fun tick() {
-        if (gui != null) {
-            try {
-                UMinecraft.getMinecraft().displayGuiScreen(gui)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-            gui = null
+        if (UMinecraft.getMinecraft().currentScreen is GuiMainMenu) {
+            UMinecraft.getMinecraft().displayGuiScreen(ExamplesGui())
         }
     }
 
     companion object {
         const val MOD_ID = "examplemod"
         const val MOD_VERSION = "1.0"
-        var gui: UScreen? = null
     }
 }
---
+++ src/main/java/com/example/examplemod/ExamplesGui.kt
@@ -0,0 +1,59 @@
+package com.example.examplemod
+
+import gg.essential.elementa.WindowScreen
+import gg.essential.elementa.components.*
+import gg.essential.elementa.constraints.*
+import gg.essential.elementa.constraints.animation.Animations
+import gg.essential.elementa.dsl.*
+import gg.essential.universal.UScreen
+import java.awt.Color
+
+/**
+ * List of buttons to open a specific example gui.
+ * See ExampleGui (singular) for a well-commented example gui.
+ */
+class ExamplesGui : WindowScreen() {
+    private val container by ScrollComponent().constrain {
+        y = 3.pixels()
+        width = 100.percent()
+        height = 100.percent() - (3 * 2).pixels()
+    } childOf window
+
+    init {
+        for ((name, action) in examples) {
+            val button = UIBlock().constrain {
+                x = CenterConstraint()
+                y = SiblingConstraint(padding = 3f)
+                width = 200.pixels()
+                height = 20.pixels()
+                color = Color(255, 255, 255, 102).toConstraint()
+            }.onMouseEnter {
+                animate {
+                    setColorAnimation(Animations.OUT_EXP, 0.5f, Color(255, 255, 255, 150).toConstraint())
+                }
+            }.onMouseLeave {
+                animate {
+                    setColorAnimation(Animations.OUT_EXP, 0.5f, Color(255, 255, 255, 102).toConstraint())
+                }
+            }.onMouseClick {
+                try {
+                    displayScreen(action())
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            } childOf container
+
+            UIText(name).constrain {
+                x = CenterConstraint()
+                y = CenterConstraint()
+            } childOf button
+        }
+    }
+
+    companion object {
+        val examples = mutableMapOf<String, () -> UScreen>(
+            "ExampleGui" to ::ExampleGui,
+            "ComponentsGui" to ::ComponentsGui,
+        )
+    }
+}
