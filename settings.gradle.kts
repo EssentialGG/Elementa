00ba5dc182ebcfbd04bd84cdbfdad4afeb326529
LlamaLad7 <l3gomindstorms@gmail.com>
2021-07-15 17:01:29 +0100

    Build: Temporarily disable 1.15.2+

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,9 +25,7 @@ rootProject.buildFileName = "root.gradle.kts"
 
 listOf(
     "1.8.9",
-    "1.12.2",
-    "1.15.2",
-    "1.16.2"
+    "1.12.2"
 //    "1.16.2-fabric"
 ).forEach { version ->
     include(":$version")
