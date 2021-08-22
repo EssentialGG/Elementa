873efb075422626cdf0d4c0c2893aa41eb9b6d03
Sychic <47618543+Sychic@users.noreply.github.com>
2021-08-22 11:42:22 -0400

    Utilities: add extension properties

--- src/main/kotlin/gg/essential/elementa/dsl/utilities.kt
+++ src/main/kotlin/gg/essential/elementa/dsl/utilities.kt
@@ -12,22 +12,33 @@ fun String.width(textScale: Float = 1f, fontProvider: FontProvider = DefaultFont
     fontProvider.getStringWidth(this, 10f) * textScale
 
 @JvmOverloads
-fun Number.pixels(alignOpposite: Boolean = false, alignOutside: Boolean = false): PixelConstraint {
-    return PixelConstraint(this.toFloat(), alignOpposite, alignOutside)
-}
+fun Number.pixels(alignOpposite: Boolean = false, alignOutside: Boolean = false): PixelConstraint =
+    PixelConstraint(this.toFloat(), alignOpposite, alignOutside)
 
-// For 1.pixel()
+val Number.pixels: PixelConstraint
+    get() = pixels(alignOpposite = false, alignOutside = false)
+
+// For 1 pixel
 @JvmOverloads
 fun Number.pixel(alignOpposite: Boolean = false, alignOutside: Boolean = false) = pixels(alignOpposite, alignOutside)
+val Number.pixel: PixelConstraint
+    get() = pixels
 
 fun Number.percent() = RelativeConstraint(this.toFloat() / 100f)
+val Number.percent: RelativeConstraint
+    get() = this.percent()
+
 fun Number.percentOfWindow() = RelativeWindowConstraint(this.toFloat() / 100f)
+val Number.percentOfWindow: RelativeWindowConstraint
+    get() = this.percentOfWindow()
 
 fun SuperConstraint<Float>.floor() = RoundingConstraint(this, RoundingConstraint.Mode.Floor)
 fun SuperConstraint<Float>.ceil() = RoundingConstraint(this, RoundingConstraint.Mode.Ceil)
 fun SuperConstraint<Float>.round() = RoundingConstraint(this, RoundingConstraint.Mode.Round)
 
 fun Color.toConstraint() = ConstantColorConstraint(this)
+val Color.constraint: ConstantColorConstraint
+    get() = this.toConstraint()
 
 operator fun Color.component1() = red
 operator fun Color.component2() = green
