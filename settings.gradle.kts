7ed13ca4c44617eb0f3004cde4e854084988cb18
Jonas Herzig <jonas@spark-squared.com>
2022-06-07 10:12:51 +0200

    Build: Update essential-gradle-toolkit
    Fixes IntelliJ imploding when it tries to analyze code referencing
    reference-stripped Kotlin classes (i.e. `common` code).

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
         maven("https://repo.essential.gg/repository/maven-public")
     }
     plugins {
-        val egtVersion = "0.1.6"
+        val egtVersion = "0.1.9"
         id("gg.essential.defaults") version egtVersion
         id("gg.essential.multi-version.root") version egtVersion
         id("gg.essential.multi-version.api-validation") version egtVersion
