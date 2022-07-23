64ebfd3b8a97bcd09bbd32505447eca3da5d4ec9
Mitchell Katz <Sk1er@sk1er.club>
2022-07-23 10:28:01 -0400

    DefaultLoadingImage: Use linear min and mag filter interpolation.
    The default is nearest, but the quality of the rendered image is much worse unless you are rendering at the exact image resolution (512x512) which is unlikely.

--- src/main/kotlin/gg/essential/elementa/components/image/DefaultLoadingImage.kt
+++ src/main/kotlin/gg/essential/elementa/components/image/DefaultLoadingImage.kt
@@ -4,6 +4,7 @@ import gg.essential.elementa.utils.drawTexture
 import gg.essential.universal.UGraphics
 import gg.essential.universal.UMatrixStack
 import gg.essential.universal.utils.ReleasedDynamicTexture
+import org.lwjgl.opengl.GL11
 import java.awt.Color
 import java.awt.image.BufferedImage
 import javax.imageio.ImageIO
@@ -12,12 +13,29 @@ object DefaultLoadingImage : ImageProvider {
     private var loadingImage: BufferedImage? = ImageIO.read(this::class.java.getResourceAsStream("/loading.png"))
     private lateinit var loadingTexture: ReleasedDynamicTexture
 
-    override fun drawImage(matrixStack: UMatrixStack, x: Double, y: Double, width: Double, height: Double, color: Color) {
+    override fun drawImage(
+        matrixStack: UMatrixStack,
+        x: Double,
+        y: Double,
+        width: Double,
+        height: Double,
+        color: Color,
+    ) {
         if (!::loadingTexture.isInitialized) {
             loadingTexture = UGraphics.getTexture(loadingImage!!)
             loadingImage = null
         }
 
-        drawTexture(matrixStack, loadingTexture, color, x, y, width, height)
+        drawTexture(
+            matrixStack,
+            loadingTexture,
+            color,
+            x,
+            y,
+            width,
+            height,
+            textureMinFilter = GL11.GL_LINEAR,
+            textureMagFilter = GL11.GL_LINEAR,
+        )
     }
 }
