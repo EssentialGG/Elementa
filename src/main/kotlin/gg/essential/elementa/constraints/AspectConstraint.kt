012beb277e5649556c83ae892815bc569b3cf71f
Mitchell Katz <Sk1er@sk1er.club>
2022-08-11 16:44:06 -0400

    AspectConstraint: Use state to track value

--- src/main/kotlin/gg/essential/elementa/constraints/AspectConstraint.kt
+++ src/main/kotlin/gg/essential/elementa/constraints/AspectConstraint.kt
@@ -2,6 +2,8 @@ package gg.essential.elementa.constraints
 
 import gg.essential.elementa.UIComponent
 import gg.essential.elementa.constraints.resolution.ConstraintVisitor
+import gg.essential.elementa.state.BasicState
+import gg.essential.elementa.state.State
 
 /**
  * For size:
@@ -10,29 +12,32 @@ import gg.essential.elementa.constraints.resolution.ConstraintVisitor
  * For position:
  * Sets the x/y position to be [value] multiple of its own y/x position respectively.
  */
-class AspectConstraint @JvmOverloads constructor(val value: Float = 1f) : PositionConstraint, SizeConstraint {
+class AspectConstraint(private val value: State<Float>) : PositionConstraint, SizeConstraint {
+
+    @JvmOverloads constructor(value: Float = 1f) : this(BasicState(value))
+
     override var cachedValue = 0f
     override var recalculate = true
     override var constrainTo: UIComponent? = null
 
     override fun getXPositionImpl(component: UIComponent): Float {
-        return (constrainTo ?: component).getTop() * value
+        return (constrainTo ?: component).getTop() * value.get()
     }
 
     override fun getYPositionImpl(component: UIComponent): Float {
-        return (constrainTo ?: component).getLeft()* value
+        return (constrainTo ?: component).getLeft()* value.get()
     }
 
     override fun getWidthImpl(component: UIComponent): Float {
-        return (constrainTo ?: component).getHeight() * value
+        return (constrainTo ?: component).getHeight() * value.get()
     }
 
     override fun getHeightImpl(component: UIComponent): Float {
-        return (constrainTo ?: component).getWidth() * value
+        return (constrainTo ?: component).getWidth() * value.get()
     }
 
     override fun getRadiusImpl(component: UIComponent): Float {
-        return (constrainTo ?: component).getRadius() * value
+        return (constrainTo ?: component).getRadius() * value.get()
     }
 
     override fun visitImpl(visitor: ConstraintVisitor, type: ConstraintType) {
