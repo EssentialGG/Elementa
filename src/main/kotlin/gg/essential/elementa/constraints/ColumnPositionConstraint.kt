1d3a98beb579315921448f44d6513890457d804f
Mitchell Katz <Sk1er@sk1er.club>
2022-09-28 20:18:24 -0400

    ColumnPositionConstraint: Add documentation and improve variable names

--- src/main/kotlin/gg/essential/elementa/constraints/ColumnPositionConstraint.kt
+++ src/main/kotlin/gg/essential/elementa/constraints/ColumnPositionConstraint.kt
@@ -8,32 +8,52 @@ import gg.essential.elementa.state.BasicState
 import gg.essential.elementa.state.State
 import org.jetbrains.annotations.ApiStatus
 
+/**
+ * Column Position Constraint is a constraint that allows the creation of a vertically
+ * align column of components. The X position of the column is determined by the
+ * finding the largest right edge of all siblings one index less than the component
+ * and adding the padding.
+ *
+ * Example:
+ * UIContainer ("Box")
+ *  - UIContainer ("Row 1")
+ *     - UIComponent ("Component 1") with width=15
+ *     - UIComponent ("Component 2") with x=ColumnPositionConstraint(10f)
+ *  - UIContainer ("Row 2")
+ *     - UIComponent ("Component 3") with width=25
+ *     - UIComponent ("Component 4") with x=ColumnPositionConstraint(10f)
+ *  The X value component 2 and 4 will be 25 + 10 = 35 pixels to the right of Box
+ *
+ */
 class ColumnPositionConstraint(
-    private val padding: State<Float>,
+    padding: State<Float>,
 ) : XConstraint, PaddingConstraint, StateRegistry {
+
+    private val paddingState = padding.map { it }
 
     constructor(padding: Float) : this(BasicState(padding))
 
     override var cachedValue = 0f
     override var recalculate = true
     override var constrainTo: UIComponent? = null
+        set(_) = throw IllegalArgumentException("Cannot constrain ColumnPositionConstraint to a component!")
 
     override fun getXPositionImpl(component: UIComponent): Float {
-        val target = component.parent
-        val index = target.children.indexOf(component) - 1
-        if (index < 0) {
+        val currentRow = component.parent
+        val previousColumnIndex = currentRow.children.indexOf(component) - 1
+        if (previousColumnIndex < 0) {
             return component.parent.getLeft()
         }
-        return target.parent.children.maxOf {
-            it.children.getOrNull(index)?.getRight() ?: 0f
-        } + padding.get()
+        return currentRow.parent.children.maxOf {
+            it.children.getOrNull(previousColumnIndex)?.getRight() ?: 0f
+        } + paddingState.get()
     }
 
     override fun visitImpl(visitor: ConstraintVisitor, type: ConstraintType) {
     }
 
     override fun getHorizontalPadding(component: UIComponent): Float {
-        return padding.get()
+        return paddingState.get()
     }
 
     override fun getVerticalPadding(component: UIComponent): Float {
@@ -43,6 +63,6 @@ class ColumnPositionConstraint(
     @ApiStatus.Internal
     @get:ApiStatus.Internal
     override val managedStates = listOf(
-        ManagedState.OfFloat(padding, "padding", true)
+        ManagedState.OfFloat(paddingState, "padding", true)
     )
 }
