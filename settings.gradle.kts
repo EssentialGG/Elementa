8d0204ae1304b1a51c5b24945181e19701899911
Jonas Herzig <jonas@spark-squared.com>
2023-09-27 11:32:36 +0200

    Build: Enable GitHub Actions based CI
    EGT is updated to fix a memory leak issue in Loom which would otherwise consume
    more memory than available on the public GitHub Actions runners.
    
    Also updates the build reference badges in the README to filter by `+` because
    our new GitHub Actions CI now uses the proper branch name (instead of `pull-#`)
    for pull request artifacts, so the current `pull` filter is insufficient.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
         maven("https://repo.essential.gg/repository/maven-public")
     }
     plugins {
-        val egtVersion = "0.2.2"
+        val egtVersion = "0.3.0"
         id("gg.essential.defaults") version egtVersion
         id("gg.essential.multi-version.root") version egtVersion
         id("gg.essential.multi-version.api-validation") version egtVersion
