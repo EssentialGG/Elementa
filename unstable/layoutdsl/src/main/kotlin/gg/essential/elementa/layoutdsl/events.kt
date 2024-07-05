ec33f4fba0f198151d026fd7f5681668911def39
sychic <47618543+Sychic@users.noreply.github.com>
2024-07-05 15:16:21 -0700

    layoutdsl: migrate `events.kt` to statev2
    Source-Commit: ea4af9df7f091cef2dcc7b9594ad6eddb41e4e3e

--- unstable/layoutdsl/src/main/kotlin/gg/essential/elementa/layoutdsl/events.kt
+++ unstable/layoutdsl/src/main/kotlin/gg/essential/elementa/layoutdsl/events.kt
@@ -2,12 +2,7 @@ package gg.essential.elementa.layoutdsl
 
 import gg.essential.elementa.UIComponent
 import gg.essential.elementa.events.UIClickEvent
-import gg.essential.elementa.state.v2.toV2
-import gg.essential.elementa.util.Tag
-import gg.essential.elementa.util.hoverScope
-import gg.essential.elementa.util.makeHoverScope
-import gg.essential.elementa.util.addTag
-import gg.essential.elementa.util.removeTag
+import gg.essential.elementa.util.*
 
 import gg.essential.elementa.state.State as StateV1
 import gg.essential.elementa.state.v2.State as StateV2
@@ -27,7 +22,7 @@ fun Modifier.hoverScope(state: StateV1<Boolean>? = null) =
     then { makeHoverScope(state); { throw NotImplementedError() } }
 
 /** Declare this component and its children to be in a hover scope. See [makeHoverScope]. */
-fun Modifier.hoverScope(state: gg.essential.elementa.state.v2.State<Boolean>) =
+fun Modifier.hoverScope(state: StateV2<Boolean>) =
     then { makeHoverScope(state); { throw NotImplementedError() } }
 
 /**
@@ -48,13 +43,13 @@ fun Modifier.inheritHoverScope() =
  * A [Modifier.hoverScope] is **require** on the component or one of its parents.
  */
 fun Modifier.whenHovered(hoverModifier: Modifier, noHoverModifier: Modifier = Modifier): Modifier =
-    then { Modifier.whenTrue(hoverScope(), hoverModifier, noHoverModifier).applyToComponent(this) }
+    then { Modifier.whenTrue(hoverScopeV2(), hoverModifier, noHoverModifier).applyToComponent(this) }
 
 /**
  * Provides the [hoverScope] to be evaluated in a lambda which returns a modifier
  */
 fun Modifier.withHoverState(func: (StateV2<Boolean>) -> Modifier) =
-    then { func(hoverScope().toV2()).applyToComponent(this) }
+    then { func(hoverScopeV2()).applyToComponent(this) }
 
 /** Applies a Tag to this component. See [UIComponent.addTag]. */
 fun Modifier.tag(tag: Tag) = then { addTag(tag); { removeTag(tag) } }
