c9ab3af4149a8b482911338b166ac29d7d3132f1
Jonas Herzig <jonas@spark-squared.com>
2024-10-02 12:29:39 +0200

    StateV2: Fix `await` cancellation
    Previous implementation would simply drop the unregister function, which
    works only under the incorrect assumption that coroutines will drop all
    resume calls after cancellation.
    However, coroutines are smarter than that, and while they will ignore
    the first resume call if the job was already cancelled, it will still
    verify the resume-must-only-be-called-once contract even in those cases
    and yell at you if you call it a second time.
    
    This commit fixes that by not dropping unregister in general but a
    separate reference specifically for this task. The effect can then still
    unregister itself after it was called for the first time and won't be
    called a second time.
    
    Source-Commit: 5fedf3858046e0ca7e5d5607e62a20504dea56f4

--- unstable/statev2/src/main/kotlin/gg/essential/elementa/state/v2/coroutine.kt
+++ unstable/statev2/src/main/kotlin/gg/essential/elementa/state/v2/coroutine.kt
@@ -17,22 +17,34 @@ suspend fun <T> State<T>.await(accept: (T) -> Boolean): T {
 
     // Slow path
     return suspendCancellableCoroutine { continuation ->
+        // We need to have the continuation carry a reference to the unregister function (we do this via
+        // invokeOnCancellation at the end of this block), so the effect is kept alive at least as long as the coroutine
+        // has any interest in it.
+        // The same reference also serves as a cancellation indicator: If it's been set to `null`, the continuation
+        // was cancelled and we can unregister the effect the next time it's called.
+        var referenceFromContinuation: Any? = null
+
         var unregister: (() -> Unit)? = null
         unregister = onChange(ReferenceHolder.Weak) { value ->
+            if (referenceFromContinuation == null) {
+                unregister?.invoke()
+                unregister = null
+                return@onChange
+            }
             if (accept(value)) {
                 unregister?.invoke()
                 unregister = null
                 continuation.resume(value)
             }
         }
+
+        referenceFromContinuation = unregister
         continuation.invokeOnCancellation {
             // Note: we cannot call `unregister` here because `invokeOnCancellation` makes no guarantee about which
             // thread we run on, and `unregister` isn't thread safe.
             // So we'll instead merely drop our reference to the unregister function and leave it to State's weakness
-            // properties to clean up the registration.
-            // This does mean our callback will continue to be invoked, but `CancellableCoroutine` is fine with that
-            // because cancellation may race with `resume` in pretty much any code.
-            unregister = null
+            // properties (or the next onChange invocation) to clean up the registration.
+            referenceFromContinuation = null
         }
     }
 }
