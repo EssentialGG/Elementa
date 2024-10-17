0b2f25dc06a54af0b11abd7ef92f7dfa30d95f72
Jonas Herzig <jonas@spark-squared.com>
2024-10-17 09:16:13 +0200

    StateV2: Add effectOnChange ListState extensions
    Source-Commit: c1067a62cc965910054f9d98dc52a2699679ee4d

--- unstable/statev2/src/main/kotlin/gg/essential/elementa/state/v2/collections/utils.kt
+++ unstable/statev2/src/main/kotlin/gg/essential/elementa/state/v2/collections/utils.kt
@@ -2,6 +2,7 @@ package gg.essential.elementa.state.v2.collections
 
 import gg.essential.elementa.state.v2.ReferenceHolder
 import gg.essential.elementa.state.v2.ListState
+import gg.essential.elementa.state.v2.effect
 
 // FIXME this is assuming there are no duplicate keys (good enough for now)
 fun <T, K, V> ListState<T>.asMap(owner: ReferenceHolder, block: (T) -> Pair<K, V>): Map<K, V> {
@@ -28,6 +29,35 @@ fun <T, K, V> ListState<T>.asMap(owner: ReferenceHolder, block: (T) -> Pair<K, V
         }
     }
     return map
+}
+
+fun <T> ListState<T>.effectOnChange(
+    referenceHolder: ReferenceHolder,
+    onChange: (TrackedList.Change<T>) -> Unit,
+) {
+    var oldList = trackedListOf<T>()
+    effect(referenceHolder) {
+        val newList = this@effectOnChange()
+        val changes = newList.getChangesSince(oldList).also { oldList = newList }
+        for (change in changes) {
+            onChange(change)
+        }
+    }
+}
+
+fun <T> ListState<T>.effectOnChange(
+    referenceHolder: ReferenceHolder,
+    add: (IndexedValue<T>) -> Unit,
+    remove: (IndexedValue<T>) -> Unit,
+    clear: (List<T>) -> Unit = { list -> list.forEach { remove(IndexedValue(0, it)) } },
+) {
+    effectOnChange(referenceHolder) { change ->
+        when (change) {
+            is TrackedList.Add -> add(change.element)
+            is TrackedList.Remove -> remove(change.element)
+            is TrackedList.Clear -> clear(change.oldElements)
+        }
+    }
 }
 
 fun <T> trackedListOf(vararg elements: T) : TrackedList<T> = MutableTrackedList(elements.toMutableList())
