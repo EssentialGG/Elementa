bf0efc91fd979f989ef43df8cd28cdb2e093528a
Jonas Herzig <jonas@spark-squared.com>
2025-01-22 19:00:46 +0100

    StateV2: Add `asyncMap` combinator
    Source-Commit: f02f6a4821a87c41b30473cae9de1aed2e7aaddc

--- unstable/statev2/src/main/kotlin/gg/essential/elementa/state/v2/coroutine.kt
+++ unstable/statev2/src/main/kotlin/gg/essential/elementa/state/v2/coroutine.kt
@@ -1,6 +1,10 @@
 package gg.essential.elementa.state.v2
 
 import gg.essential.elementa.state.v2.ReferenceHolder
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlin.coroutines.resume
 
@@ -95,4 +99,55 @@ interface StateIterable<T> {
 interface StateIterator<T> {
     suspend operator fun hasNext(): Boolean
     operator fun next(): T
+}
+
+/**
+ * Returns a new [State] which contains result of applying the given suspending [block] to the value of `this` [State].
+ * The returned [State] will return `null` while the function is suspended.
+ * When the value of `this` [State] changes, the suspending function is cancelled and a new one is launched.
+ *
+ * If [previousWhileWorking] is `true` and the value of `this` [State] changes, the returned [State] will continue to
+ * provide the latest result (if any) instead of returning `null`. Consequently, if the given [block] never returns
+ * `null`, the returned [State] will only ever be `null` during the initial load.
+ *
+ * If the given [block] returns a result without suspending, the returned [State] is guaranteed to immediately return
+ * this value, and does not suffer from the "Recursion" issue described in [effect]'s KDocs.
+ * Note that to this end, the [block] is launched on the given [scope] with [CoroutineStart.UNDISPATCHED], so the
+ * dispatcher on this [scope] will not be respected until the first suspension point. However, given [State] is not
+ * generally thread-safe, the given dispatcher must dispatch to the same thread as the thread currently evaluating the
+ * State anyway, so this should not be an issue in practice.
+ */
+fun <I, R> State<I>.asyncMap(scope: CoroutineScope, previousWhileWorking: Boolean = true, block: suspend (I) -> R): State<R?> {
+    val prevResultState = mutableStateOf<Pair<I, R>?>(null)
+    var jobInput: I? = null
+    var job: Job? = null
+    return State {
+        val input = this@asyncMap()
+
+        // If we have a previous result and the input is unchanged, we can just use that
+        val prevResult = prevResultState()
+        if (prevResult != null && prevResult.first == input) {
+            return@State prevResult.second
+        }
+
+        // Otherwise we need to go compute the result for the new input, unless we're already doing that of course
+        if (jobInput != input) {
+            jobInput = input
+            job?.cancel()
+            job = scope.launch(start = CoroutineStart.UNDISPATCHED) {
+                val result = block(input)
+                prevResultState.set(Pair(input, result))
+            }
+
+            // If the block returned a result without ever suspending, then we can immediately make use of that
+            // result.
+            val latestResult = prevResultState()
+            if (latestResult != null && latestResult.first == input) {
+                return@State latestResult.second
+            }
+        }
+
+        // Computation in progress, best we can do is provide the previous value if that's acceptable
+        return@State if (previousWhileWorking) prevResult?.second else null
+    }
 }
